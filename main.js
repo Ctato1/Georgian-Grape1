684a5df19d1e7c52823d8e3189c3ba14293fa627
Ctato1 <86792981+Ctato1@users.noreply.github.com>
2023-06-20 15:46:56 +0400

    finnal

--- main.js
+++ main.js
@@ -26,3 +26,139 @@ profileImg.addEventListener("click", () => {
     proFlag = false;
   }
 });
+
+// wines page
+
+const cardDiv = document.querySelector("#card-div");
+
+const product_01 =
+  "https://www.topgeorgian.wine/wp-content/uploads/2022/11/WT0089-menabde-wines-khashmis-saperavi-2021.jpg";
+
+const product_02 =
+  "https://www.topgeorgian.wine/wp-content/uploads/2022/07/WT0076-khmelos-marani-shavkapito-kasris-2021.jpg";
+
+const product_03 =
+  "https://www.topgeorgian.wine/wp-content/uploads/2022/11/QV0138-niamori-chinuri-qvevri-2021-600x600.jpg";
+
+const product_04 =
+  "https://www.topgeorgian.wine/wp-content/uploads/2022/11/QV0139-niamori-rkatsiteli-qvevri-2021-600x600.jpg";
+
+const product_05 =
+  "https://www.topgeorgian.wine/wp-content/uploads/2022/11/QV0144-niamori-tavkveri-qvevri-2021-600x600.jpg";
+
+const products = [
+  {
+    id: "01",
+    title: "Khashmis Saperavi",
+    geoName: "ხაშმის საფერავი",
+    price: 27.0,
+    imgUrl: product_01,
+
+    category: "dry red",
+
+    desc: `წითელი მშრალი ღვინო წითელი და შავი კენკრის ხილის, მაყვლის, მოცვის, ანწლის არომატებით და სუსტად გამოხატული სანელებლების ტონებით. რეკომენდირებულია მისი მირთმევა ნანადირევ ან წითელ ხორცთან, შესუნელებულ ღორის ხორცთან, საქონლის ხორცთან, ფრინველის შემწვარ ხორცთან და მაგარ ყველთან ერთად.`,
+  },
+
+  {
+    id: "02",
+    title: "Shavkapito",
+    geoName: "შავკაპიტო",
+    price: 25.0,
+    imgUrl: product_02,
+
+    category: "dry red",
+
+    desc: `
+    წითელი მშრალი კასრის ღვინო, მაყვლის, მოცხარის, ალუბლის, შავი კენკრის და შავი ქლიავის ჩირის არომატებით. რეკომენდირებულია მისი მირთმევა ხბოს ხორცთან, ფრინველის ხორცთან, ლორთან, ხამონთან, ნანადირევთან, პასტასთან და ყველთან ერთად.`,
+  },
+
+  {
+    id: "03",
+    title: "Chinuri",
+    geoName: "ჩინური",
+    price: 22.0,
+    imgUrl: product_03,
+
+    category: "dry white",
+
+    desc: `
+    თეთრი მშრალი ქვევრის ღვინო ატმის, მსხლის, თეთრი კენკროვანი ხილის, კომშის და ჩირების არომატებით. გემოზე სასიამოვნო დაბოლოებით. რეკომენდირებულია მისი მირთმევა ფრინველის ხორცთან, ზღვის პროდუქტებთან, ხილთან და მაგარ ყველთან ერთად.`,
+  },
+
+  {
+    id: "04",
+    title: "Rkatsiteli",
+    geoName: "რქაწითელი",
+    price: 18.0,
+    imgUrl: product_04,
+
+    category: "dry white",
+
+    desc: `
+    თეთრი მშრალი ქვევრის ღვინო ტროპიკური ხილის, მინდვრის ყვავილების, კომშის, ატმის, გულაბი მსხლის და ჩირების არომატებით. რეკომენდირებულია მისი მირთმევა თეთრ ხორცთან, თევზეულთან, ბოსტნეულთან და მაგარ ყველთან ერთად.`,
+  },
+
+  {
+    id: "05",
+    title: "Tavkveri",
+    geoName: "თავკვერი",
+    price: 18.0,
+    imgUrl: product_05,
+
+    category: "dry white",
+
+    desc: `
+წითელი მშრალი ქვევრის ღვინო წითელი ხილის, ჟოლოს, მოცვის, ალუბლის, კოწახურის და ბროწეულის ყვავილის არომატებით. რეკომენდირებულია მისი მირთმევა შემწვარ ფრინველის ხორცთან, ორაგულის სტეიკთან, ღორის ხორცთან და მაგარ ყველთან ერთად.`,
+  },
+];
+
+function renderCard(changedCard) {
+  if (!changedCard || changedCard === "all") {
+    cardDiv.innerHTML = "";
+    for (let i = 0; i < products.length; i++) {
+      let { id, title, geoName, price, imgUrl, category, desc } = products[i];
+
+      cardDiv.innerHTML += `
+  <div class="type__card">
+    <img src=${imgUrl} alt="card" />
+    <div class="type__card-info">
+      <h5>${geoName}</h5>
+      <p>${desc}</p>
+      <p class="type__card-price">${price} ლარი</p>
+    </div>
+  </div>
+    
+  `;
+    }
+  } else {
+    cardDiv.innerHTML = "";
+    let filteredItem = products.filter((e) => e.category === changedCard);
+    if (filteredItem.length === 0) {
+      cardDiv.innerHTML = `
+      <h1 class='not-found mt-5 mb-5 w-100 d-flex justify-content-center text-light'>No Products are found!</h1>
+      `;
+    }
+    filteredItem.map(
+      (item, index) =>
+        (cardDiv.innerHTML += `
+  <div class="type__card">
+    <img src=${item.imgUrl} alt="card" />
+    <div class="type__card-info">
+      <h5>${item.geoName}</h5>
+      <p>${item.desc}</p>
+      <p class="type__card-price">${item.price} ლარი</p>
+    </div>
+  </div>
+    
+  `)
+    );
+  }
+}
+
+function changeItem() {
+  var x = document.getElementById("mySelect").value;
+  console.log(x);
+  renderCard(x);
+}
+
+renderCard();
